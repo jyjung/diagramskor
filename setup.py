e4f45b63a6e3224f4ffbbe8c995f45f7799f0b5c
Jung Jinyoung <bungker@gmail.com>
2024-11-11 12:52:39 +0900

    update

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ import os
 
 setup(
     name='diagramskor',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     include_package_data=True,
     package_data={
-        'my_package': [],  # 패키지 내에 데이터 파일을 포함하지 않음
+        'diagramskor': ['*.png'],  # 패키지 내에 데이터 파일을 포함하지 않음
     },
-    data_files= [],  # /site-packages에 복사할 파일    
     install_requires=['diagrams'],
     author='Jung Jin Young',
     author_email='bungker@gmail.com',
